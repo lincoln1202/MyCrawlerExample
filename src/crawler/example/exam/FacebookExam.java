40de24f88bf8726daeeabb5e8cc2071338d32b0a
Lincoln Tong <a27072041@gamil.com>
2016-12-10 16:27:48 +0800

    FB(reactions)

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -24,9 +24,9 @@ public class FacebookExam {
 		// 遠端資料路徑
 
 		String uri = 
-				"https://graph.facebook.com/v2.6"
-				+ "/search?q="
-				+ "&access_token=";
+				"https://graph.facebook.com/v2.8"
+				 +"/crazyck101/posts?fields=id,link,message,reactions.type(HAHA).limit(0).summary(total_count)&since=1480849200&until=1480856400"
+				+ "&access_token=EAACEdEose0cBAO8qWCrA5ZA0Kv8ber4t1iZCm1F7uo95OFpda9PYKwOI8vC4bEZBNJSlBrAsSsaYVutMsXskpAyxfBCpw8NBXdwCAjK0pRYMZB6ZCpA6ZB8QjUG9hHgZAHVDhtXMUMwLMRlARbWpSYljjRwqBpLQs2sxafCmVcRfAZDZD";
 
 
 		Elements elems =
@@ -34,17 +34,19 @@ public class FacebookExam {
 				.getFromJson(uri)
 				.select("data");
 		
-		String output = "id,reactions";
+		String output = "id,按讚(HAHA)人數"+"\n";
 
 		// 遂筆處理
 		for( Element data: elems ){
 			String id = data.select("id").text();
+			String message = data.select("total_count").text();
+
 
 			// FIXIT
-			String reactions = "";
 
 
-			output += id + "," + reactions + "\n";
+
+			output += id + ","+message+ "\n";
 		}
 
 		System.out.println( output );
